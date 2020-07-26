a581723f40e210025d1eda464f4a63188f3a8941
Narendra <narendra.vadde@zee.esselgroup.com>
2020-07-26 10:39:13 +0530

    device orienattion

--- Device Orientation/ViewController.swift
+++ Device Orientation/ViewController.swift
@@ -12,9 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(orientationChanged(_:)), name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+    
+    @objc func orientationChanged(_ notification: Notification) {
+        let device = UIDevice.current
+        switch device.orientation {
+        case .portrait:
+            print("portrait")
+        case .portraitUpsideDown:
+            print("portraitUpsideDown")
+        case .landscapeLeft:
+            print("landscapeLeft")
+        case .landscapeRight:
+            print("landscapeRight")
+        case .faceUp:
+            print("faceUp")
+        case .faceDown:
+            print("faceDown")
+        case .unknown:
+            print("unknown")
+        default:
+            break
+        }
     }
 
-
 }
-
