6bb7d74257d55a7d3e04978ce14b4bfc0b872c0a
Narendra <narendra.vadde@zee.esselgroup.com>
2020-07-26 10:31:09 +0530

    Initial Commit

---
+++ Device Orientation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Device Orientation
+//
+//  Created by narendra. vadde on 26/07/20.
+//  Copyright © 2020 narendra. vadde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
